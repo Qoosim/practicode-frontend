e64fde28b544bc6930aed3ab8e1a58e9ff7879e5
Dbax <123155596+diremu@users.noreply.github.com>
2023-11-22 08:52:49 +0100

    Minor changes made

--- src/components/Why-us/Why-us.js
+++ src/components/Why-us/Why-us.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import './Why-us.css';
 import searchIcon from './Images/search-icon.jpg';
 import monitorLogo from './Images/monitor-logo.png'
@@ -7,7 +7,12 @@ import databaseLogo from  './Images/database-logo.png'
 import bracketsLogo from  './Images/angle-brackets-logo.png'
 
 const WhyUs = () => {
-
+    //Using the useState so that the button that is clicked would be yellow and the others would be grey
+    const [onClick, onClicked] = useState(false)
+    const buttonActivity = () => {
+        onClicked(!onClick)
+        return 0;
+    }
     return (
         <div id="main-body">
             <div id="sub-div1">
@@ -16,9 +21,10 @@ const WhyUs = () => {
             </div>
             <div id="sub-div2">
                 <div id="sub-div2-1">
-                    <button type="button" id="all" className="course-btn" >All</button>
-                    <button type="button" id="popular" className="course-btn">Popular</button>
-                    <button type="button" id="catalogue" className="course-btn active" >Catalogue</button>
+                    {/*This useState is still broken in the fact that activating one button activates all and deactivating one does the same.*/}
+                    <button type="button" id="all" onClick={buttonActivity} className={`course-btn ${onClick ? 'active' : ''}`} >All</button>
+                    <button type="button" id="popular" onClick={buttonActivity}  className={`course-btn ${onClick ? 'active' : ''}`}>Popular</button>
+                    <button type="button" id="catalogue" onClick={buttonActivity} className={`course-btn ${onClick ? 'active' : ''}`} >Catalogue</button>
                 </div>
                 <div id="sub-div2-2">
                     <form>
@@ -29,6 +35,7 @@ const WhyUs = () => {
             </div>
             <div id="sub-div3">
                 <div className="enrollment-cards">
+                    {/*TODO:Replace Smartphone logo with one of a higher resolution*/}
                     <img src={phoneLogo} alt="A logo of a smartphone depicting Web and Mobile Development" id="phoneLogo" />             
                     <h5 className="enrollment-heading">Web & Mobile Development</h5>
                     <br />
